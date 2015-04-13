0b2a83c2fef67d7de522ea5ea1bcf73ad5d38ea2
root <kjohnson@aerisnetwork.com>
2015-04-13 14:45:31 -0400

    Add rapidssl CA bundles and a ioncube test file

---
+++ files/ioncubetest.php
@@ -0,0 +1,14 @@
+<?php //0048e
+// Software written by myEASYwp.com
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo('Site error: the file <b>'.__FILE__.'</b> requires the ionCube PHP Loader '.basename($__ln).' to be installed by the website operator. If you are the website operator please use the <a href="http://www.ioncube.com/lw/">ionCube Loader Wizard</a> to assist with installation.');exit(199);
+?>
+HR+cPx0XSUhozqdEZvlaoIi7zrnfSNvbZ0BbMifQ9l0S+bzY98TruMPmYBVB0vpVYY+sgDYxDoFA
+6jWW2GdPqnKRElWkLiD45b9GiqLrfKnAl4q9ZoEpK4zBch+oJMkIdWQEH6BGK/V96doAkknSI4ev
+7dOHKxBHWs5wdm23+GaTM85/fS4562Ee7OvGzNDB8pNqCx5U9iDRv9tfHeXlNJ+R6MXO4LUYZryD
+mfidS6glcMq/1BW6x2Csb1O3OWbiQ+RVkdvfk/ZQDuVF/qNJ763lhZtk19Ra3KojVsJW2Eb0jhus
+Eo2XrXjwPt7/VrVnc5439lSPxRmU3W1RiaHpceCYoTsz496ZKphRGj9RSilYw/q8q1NUVauvguqI
+WKt9QxGAQqNoi3xgieICME0dZfcHxwgQcLv4P/Jkh/JBTCoN/1oHUD+cN0xWKyZr0dv2n8zs6SHA
+/Lds4XGKvqipiQV30ImvAiPgYKDBl6Y+0KIvHKvmWd0hDGfa6mMmmjKQAEeU36pnjPwbFgG79Uhc
+cNIj5m+6Op6J+T/qAp3Ac+yDUjug0UZ3+5CateYaVTE5oKon4lpCv0mLZirMqOJnU+dQZB2PRY4v
+eHXgkBkQsj0B4GYF6j6AUYKMxKf6KEXXyVubjuEmNBiPZDIMRpkOcHQnfuGGhT53+KEIkPQXvW7Q
+AbOj2/5rx9H8NCOYRfVYQ2ZK3OdUNG+N0gTiHUaJfp8nrBGbLMEJIRQadkZq
